19d0d734e873451c84ddc8a436f855f20a70fa66
OMARI Ayoub <kali@kali>
2026-01-22 10:11:48 +0100

    fix botpress script import race condition

--- components/BotpressChat.tsx
+++ components/BotpressChat.tsx
@@ -1,15 +1,28 @@
+"use client";
+
 import Script from "next/script";
+import { useState } from "react";
 
 export default function BotpressChat() {
+  const [libraryLoaded, setLibraryLoaded] = useState(false);
+
   return (
     <>
-      <Script src="https://cdn.botpress.cloud/webchat/v3.5/inject.js" />
-
+      {/* 1. Load the main Botpress Inject library first */}
       <Script
-        src="https://files.bpcontent.cloud/2026/01/15/16/20260115160015-Q3KUXXOF.js"
-        strategy="lazyOnload"
-        defer
+        src="https://cdn.botpress.cloud/webchat/v3.5/inject.js"
+        onLoad={() => {
+          setLibraryLoaded(true);
+        }}
       />
+
+      {/* 2. Only load the configuration script once the library is ready */}
+      {libraryLoaded && (
+        <Script
+          src="https://files.bpcontent.cloud/2026/01/15/16/20260115160015-Q3KUXXOF.js"
+          strategy="afterInteractive"
+        />
+      )}
     </>
   );
 }
